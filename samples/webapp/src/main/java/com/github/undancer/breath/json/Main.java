04a3ffc2092b2ea0708d7d0b3de8fb2f63ec56ba
undancer <undancer@gmail.com>
2013-11-09 02:57:47 +0800

    修改并整理配置文件，这一次为其添加类似maven的provided等scope

---
+++ samples/webapp/src/main/java/com/github/undancer/breath/json/Main.java
@@ -0,0 +1,12 @@
+package com.github.undancer.breath.json;
+
+/**
+ * Created by undancer on 13-11-9.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        javax.servlet.Htt
+//        HttpServletRequest
+    }
+}
