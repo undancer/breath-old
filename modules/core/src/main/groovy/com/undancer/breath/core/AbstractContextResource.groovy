e4fa02390d63bec8e3a6da547c13a1ba828a1f8b
undancer <undancer@gmail.com>
2013-11-10 00:50:20 +0800

    添加security

---
+++ modules/core/src/main/groovy/com/undancer/breath/core/AbstractContextResource.groovy
@@ -0,0 +1,11 @@
+//package com.undancer.breath.core
+//
+///**
+// * Created by undancer on 13-11-7.
+// */
+//class AbstractContextResource implements MetricProcessor {
+//
+//    public static void main(String[] args) {
+//        MetricProcessor
+//    }
+//}
